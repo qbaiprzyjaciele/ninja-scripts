b9f8a952f5ffed500f9e7f19a9e883c57c9e4316
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2017-10-25 01:01:26 +0200

    parsing rest templates

--- powershell/powershell-http-server.ps1
+++ powershell/powershell-http-server.ps1
@@ -1,12 +1,36 @@
 
-$hostName = "localhost";
-$port = 8087;
 
-$httpListener = New-Object System.Net.HttpListener;
-$baseAddr = "http://" + $hostName + $port + "/";
-$httpListener.Prefixes.Add("http://" + $hostName + $port + "/");
+function startServer() {
+    $hostName = "localhost";
+    $port = 8087;
 
-$httpListener.Start();
+    $httpListener = New-Object System.Net.HttpListener;
+    $baseAddr = "http://" + $hostName + $port + "/";
+    $httpListener.Prefixes.Add("http://" + $hostName + $port + "/");
+
+    $httpListener.Start();
+}
+
+function startAction($pathParams, $queryParams) {
+    echo "lolz";
+}
+
+function addAction() {
+
+}
+
+$actions = @{
+    "/start" = startAction;
+    "/add/{id}" = addAction;
+    "/clients/{id}/products/{pid}/lol/{quack}" = "";
+    "/troloo/{id}/products/{pid}/lol/{hej}/nocotam" = "";
+}
+
+$expressions = ("/add/{id}","/start","/clients/{id}/products/{pid}/lol/{quack}");
+
+$paramNames = @{};
+$actions.Keys | %{ $paramNames.Add($_,  $( $_ | sls -allmatches "\{[a-zA-Z]*\}" |  %{ $_.Matches} | %{ $_.Value.Substring(1,$_.Value.Length-2); }))};
+
 
 
 
