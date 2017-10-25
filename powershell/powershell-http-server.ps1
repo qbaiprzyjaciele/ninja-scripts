822a94da09029df4d0b9d456953da2789d40bf7e
Jakub Smorąg <qbaiprzyjaciele@gmail.com>
2017-10-25 20:27:10 +0200

    clean up

--- powershell/powershell-http-server.ps1
+++ powershell/powershell-http-server.ps1
@@ -5,32 +5,28 @@ function startServer() {
     $port = 8087;
 
     $httpListener = New-Object System.Net.HttpListener;
-    $baseAddr = "http://" + $hostName + $port + "/";
+    $baseAddr = "http://" + $hostName + ":" + $port + "/";
     $httpListener.Prefixes.Add("http://" + $hostName + $port + "/");
-
     $httpListener.Start();
 }
 
 function startAction($pathParams, $queryParams) {
-    echo "lolz";
+    echo "start action...";
 }
 
 function addAction() {
 
 }
 
+
 $actions = @{
     "/start" = startAction;
     "/add/{id}" = addAction;
-    "/clients/{id}/products/{pid}/lol/{quack}" = "";
-    "/troloo/{id}/products/{pid}/lol/{hej}/nocotam" = "";
+    "/clients/{id}/products/{pid}/events/{eventId}" = "";
+    "/litwo/{ojczyzno}/moja/{ty}/jestes/{jak}/zdrowie" = "";
 }
-
-$expressions = ("/add/{id}","/start","/clients/{id}/products/{pid}/lol/{quack}");
 
 $paramNames = @{};
 $actions.Keys | %{ $paramNames.Add($_,  $( $_ | sls -allmatches "\{[a-zA-Z]*\}" |  %{ $_.Matches} | %{ $_.Value.Substring(1,$_.Value.Length-2); }))};
-
-
 
 
